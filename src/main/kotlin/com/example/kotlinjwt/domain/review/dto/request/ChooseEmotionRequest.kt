77f0a7a495112e5b4eeb433429b70912ad8731d6
su080915 <tbr335@naver.com>
2025-04-03 21:58:20 +0900

    fix: choose emotion req

--- src/main/kotlin/com/example/kotlinjwt/domain/review/dto/request/ChooseEmotionRequest.kt
+++ src/main/kotlin/com/example/kotlinjwt/domain/review/dto/request/ChooseEmotionRequest.kt
@@ -1,5 +1,7 @@
 package com.example.kotlinjwt.domain.review.dto.request
 
+import com.example.kotlinjwt.domain.review.model.enums.Emotion
+
 data class ChooseEmotionRequest (
-    val emotions : List<String>,
+    val emotions : List<Emotion>,
 )
